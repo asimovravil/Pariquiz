ec512d450ef0496d5010ddaacd21e924330a88b1
Ravil <star.toni@mail.ru>
2023-10-05 14:25:14 +0600

    [TASK] Build Project

---
+++ Pariquiz/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Pariquiz
+//
+//  Created by Ravil on 05.10.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.overrideUserInterfaceStyle = .dark
+      
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
