48de63584e9467f6578bfeda001ecd1f13f59eec
Ravil <star.toni@mail.ru>
2023-10-05 14:32:15 +0600

    [TASK] Edits Resources -> (Fonts, AssetHelp, AppHelp)

---
+++ Pariquiz/Resources /AppHelp/AppColor.swift
@@ -0,0 +1,31 @@
+//
+//  AppColor.swift
+//  Pariquiz
+//
+//  Created by Ravil on 05.10.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol AppColorProtocol {
+    var rawValue: String { get }
+}
+
+extension AppColorProtocol {
+
+    var uiColor: UIColor {
+        guard let color = UIColor.init(named: rawValue) else {
+            fatalError("Could not find color with name \(rawValue)")
+        }
+        return color
+    }
+
+    var cgColor: CGColor {
+        return uiColor.cgColor
+    }
+}
+
+enum AppColor: String, AppColorProtocol {
+    case whiteCustom
+}
---
+++ Pariquiz/Resources /AppHelp/AppImage.swift
@@ -0,0 +1,39 @@
+//
+//  AppImage.swift
+//  Pariquiz
+//
+//  Created by Ravil on 05.10.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol AppImageProtocol {
+    var rawValue: String { get }
+}
+
+extension AppImageProtocol {
+
+    var uiImage: UIImage? {
+        guard let image = UIImage(named: rawValue) else {
+            fatalError("Could not find image with name \(rawValue)")
+        }
+        return image
+    }
+    
+    var systemImage: UIImage? {
+        guard let image = UIImage(systemName: rawValue) else {
+            fatalError("Could not find image with name \(rawValue)")
+        }
+        return image
+    }
+    
+}
+
+enum AppImage: String, AppImageProtocol {
+    
+    // MARK: - Main
+    
+    case splashLoading
+}
+
