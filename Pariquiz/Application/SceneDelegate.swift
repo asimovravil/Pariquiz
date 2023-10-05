26e41fb9862a84e641bfd2ebb5fee1f3c4572b40
Ravil <star.toni@mail.ru>
2023-10-05 14:52:25 +0600

    [TASK] LoadingViewController (WebView & Firebase)

--- Pariquiz/Application/SceneDelegate.swift
+++ Pariquiz/Application/SceneDelegate.swift
@@ -8,19 +8,27 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
+    var navigation: UINavigationController!
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         
-        let window = UIWindow(windowScene: windowScene)
-        window.overrideUserInterfaceStyle = .dark
-      
-        window.rootViewController = SplashViewController()
-        self.window = window
-        window.makeKeyAndVisible()
-        guard let _ = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.overrideUserInterfaceStyle = .dark
+        window?.rootViewController = SplashViewController()
+        window?.makeKeyAndVisible()
+    
+        ExternalService.shared.request { [weak self] url in
+            if let url = url {
+                self?.window?.rootViewController = LoadingViewController(url: url)
+            } else {
+                if !AppStorage.isOnboardingShowed {
+                    self?.navigation = UINavigationController(
+                        rootViewController: MainViewController()
+                    )
+                }
+                self?.window?.rootViewController = self?.navigation
+            }
+        }
     }
 }
-
